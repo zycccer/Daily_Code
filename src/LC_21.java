7531abe295d11a65e20b50c37db480737f1d12bb
怪叔叔 <191462660@qq.com>
2022-03-13 22:48:34 +0800

    list

---
+++ src/LC_21.java
@@ -0,0 +1,31 @@
+
+
+public class LC_21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null)return list2;
+        if (list2 == null)return list1;
+
+        ListNode resultNode = new ListNode(0);
+        ListNode p = resultNode;
+
+        while (list1!=null && list2 != null){
+            if (list1.val < list2.val){
+                p.next = list1;
+                list1 = list1.next;
+            }else {
+                p.next = list2;
+                list2 = list2.next;
+            }
+            p = p.next;
+        }
+        if (list1 != null){
+            p.next = list1;
+        }
+        if (list2 != null){
+            p.next = list2;
+        }
+        return resultNode.next;
+
+    }
+
+}
