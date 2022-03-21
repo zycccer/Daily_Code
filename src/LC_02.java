25367bae44b9b02c38593d11ed186241b8f7675a
怪叔叔 <191462660@qq.com>
2022-03-21 23:34:10 +0800

    test

---
+++ src/LC_02.java
@@ -0,0 +1,22 @@
+public class LC_02 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sum = l1Val+l2Val+carry;
+            carry = sum/10;
+
+            ListNode sumNode = new ListNode(sum%10);
+            cursor.next = sumNode;
+            cursor = sumNode;
+
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        return root.next;
+    }
+}
