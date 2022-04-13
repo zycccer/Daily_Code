1cc814063b39cb85c7b7feb6cd25381fa1ef64a4
怪叔叔 <191462660@qq.com>
2022-04-13 20:53:46 +0800

    链表

---
+++ src/zuo_video/FindFirstIntersectNode.java
@@ -0,0 +1,99 @@
+package zuo_video;
+
+import java.security.PublicKey;
+
+public class FindFirstIntersectNode {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node getIntersectNode(Node head1,Node head2){
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+
+        if (loop1==null&& loop2 == null){
+            return  noLoop(head1,head2);
+        }
+        if (loop1 != null && loop2!=null){
+            return bothLoop(head1, loop1, head2, loop2);
+        }
+        return null;
+    }
+
+    public static Node getLoopNode(Node head){
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node n1 = head.next;
+        Node n2 = head.next.next;
+        while (n1!=n2){
+            if (n1.next == null || n2.next ==null){
+                return null;
+            }
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
+
+        n2 = head;
+
+        while (n1!=n2){
+            n1 = n1.next;
+            n2 =  n2.next;
+        }
+        return n1;
+    }
+    public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2){
+         Node cur1 = null;
+         Node cur2 = null;
+
+         if (loop1 == loop2){
+
+         }
+         return null;
+    }
+    public static Node noLoop(Node head1,Node head2)
+    {
+        if (head1==null || head2 ==null){
+            return  null;
+        }
+
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next !=null){
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next !=null){
+            n--;
+            cur2 = cur2.next;
+        }
+        if (cur1!=cur2){
+            return null;
+        }
+
+        cur1 = n > 0 ? head1:head2;
+        cur2 = cur1==head1? head2:head1;
+
+        n = Math.abs(n);
+
+        while (n!=0){
+            n--;
+            cur1 = cur1.next;
+        }
+
+        while (cur1!=cur2){
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+}
--- src/zuo_video/SmallerEqualBigger.java
+++ src/zuo_video/SmallerEqualBigger.java
@@ -33,6 +33,14 @@ public class SmallerEqualBigger {
         }
 
         arrPartition(nodeArr, pivot);
+        int j = 1;
+        for (; j < nodeArr.length; j++) {
+            nodeArr[j-1].next = nodeArr[j];
+        }
+        nodeArr[j-1].next = null;
+
+        return nodeArr[0];
+
     }
 
     public static void arrPartition(Node[] nodeArr,int pivot){
@@ -56,6 +64,80 @@ public class SmallerEqualBigger {
         Node tmp = nodeArr[a];
         nodeArr[a] = nodeArr[b];
         nodeArr[b] = tmp;
+
+    }
+
+    public static Node listPartition2(Node head,int pivot){
+        Node sH = null;
+        Node sT = null;
+        Node eH = null; // equal head
+        Node eT = null; // equal tail
+        Node bH = null; // big head
+        Node bT = null; // big tail
+
+        Node next = null; // save next node
+
+        while (head!=null){
+            next = head.next;
+            head.next = null;
+
+            if (head.value < pivot) {
+                if (sH == null) {
+                    sH = head;
+                    sT = head;
+                } else {
+                    sT.next = head;
+                    sT = head;
+                }
+            } else if (head.value == pivot) {
+                if (eH == null) {
+                    eH = head;
+                    eT = head;
+                } else {
+                    eT.next = head;
+                    eT = head;
+                }
+            } else {
+                if (bH == null) {
+                    bH = head;
+                    bT = head;
+                } else {
+                    bT.next = head;
+                    bT = head;
+                }
+            }
+                head = next;
+            }
+            if (sT!= null){
+                sT.next = eH;
+                eT = eT==null? sT:eT;
+        }
+            if (eT!=null){
+                eT.next = bH;
+            }
+
+            return sH != null ? sH:eH != null ? eH:bH;
+    }
+    public static void printLinkedList(Node node) {
+        System.out.print("Linked List: ");
+        while (node != null) {
+            System.out.print(node.value + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node head1 = new Node(7);
+        head1.next = new Node(9);
+        head1.next.next = new Node(1);
+        head1.next.next.next = new Node(8);
+        head1.next.next.next.next = new Node(5);
+        head1.next.next.next.next.next = new Node(2);
+        head1.next.next.next.next.next.next = new Node(5);
+        printLinkedList(head1);
+         head1 = listPartition(head1, 7);
+//        head1 = listPartition2(head1, 5);
+        printLinkedList(head1);
 
     }
 }
